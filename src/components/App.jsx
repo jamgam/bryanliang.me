76e0e94a1f3825d4b9504890070235641e864c7f
Bryan Liang <bryanliang1995@gmail.com>
2021-02-03 16:25:55 -0800

    (Init)

---
+++ src/components/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const App = () => {
+
+    return <div>Hello World! Welcome to my site! My name is Bryan L.</div>
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+      path: path.resolve(__dirname, 'public'),
+      filename: 'main.js'
+  },
+  resolve: {
+    // enforceModuleExtension: false,
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+}
+  
