c500122b19442623fdad3e6b26ed20a9e70bef7d
Bryan Liang <bryanliang1995@gmail.com>
2021-02-07 12:56:08 -0800

    Add cors and morgan

--- server/server.js
+++ server/server.js
@@ -1,11 +1,20 @@
 const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+
+var corsOptions = {
+  origin: 'https://www.bryanliang.me',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
+}
 
 const app = express()
 
+app.use(morgan('combined'))
+app.use(cors(corsOptions))
 const PORT = process.env.PORT || 3000
 
 app.use('/', express.static('public'))
 
 app.listen(PORT, () => {
-    console.log(`listening on *:${PORT}`)
+  console.log(`listening on *:${PORT}`)
 })
